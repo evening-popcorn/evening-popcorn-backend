def68a1eb76cd9f84159f279039a97f697289758
bitizen248 <van4011@yandex.ru>
2023-02-28 21:12:54 +0400

    fix name resolving

--- ep_client/ep_client/moviegeek/client.py
+++ ep_client/ep_client/moviegeek/client.py
@@ -9,8 +9,8 @@ class MovieGeekClient:
 
     def __init__(self) -> None:
         self.client = httpx.AsyncClient(
-            base_url=os.environ.get("CLIENT_MOVIEGEEK_URL", "http://moviegeek")
-        )
+            base_url=os.environ.get("CLIENT_MOVIEGEEK_URL",
+                                    "http://moviegeek-service"))
 
     async def get_movie(self, movie_id: int, locale: str) -> MovieInfo:
         res = await self.client.get(f"/v1/movie/{movie_id}")
